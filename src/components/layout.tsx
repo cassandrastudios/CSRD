b6d71dffd7f2d93035f4c8fc7ca79f80d01c628f
AndrasDanyi <andras@danyi.net>
2025-10-07 18:06:05 +0200

    Fix case sensitivity issue: Navigation.tsx import

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { Navigation } from './navigation'
+import { Navigation } from './Navigation'
 
 interface LayoutProps {
   children: React.ReactNode
